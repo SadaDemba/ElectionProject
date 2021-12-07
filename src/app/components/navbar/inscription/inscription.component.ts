295d12b2a0115f070c9d84aa790a5906e8530247
Sada Demba Thiam <sadadembat9@gmail.com>
2021-12-07 14:43:25 +0000

    gestion ng-if

--- src/app/components/navbar/inscription/inscription.component.ts
+++ src/app/components/navbar/inscription/inscription.component.ts
@@ -6,16 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-  expression!:boolean;
+  expression:number=0;
   constructor() { }
 
   ngOnInit(): void {
-    this.expression=true;
+    this.expression=0;
   }
 
   disponible()
   {
-    this.expression=false;
+    if(this.expression==2)
+      this.expression=0;
+    else
+      this.expression++;
 
   }
 }
