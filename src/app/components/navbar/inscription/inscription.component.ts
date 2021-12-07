099f7c42d034952b4a4896f8f74b5a296f46a6c9
abdouazizsy <ziza97tiv@gmail.com>
2021-12-07 10:09:49 +0000

    Implementattion Inscription

--- src/app/components/navbar/inscription/inscription.component.ts
+++ src/app/components/navbar/inscription/inscription.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-
+  expression!:boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.expression=true;
   }
 
+  disponible()
+  {
+    this.expression=false;
+
+  }
 }
