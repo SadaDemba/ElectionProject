cc9e0ff2ca10f894febc3c0e1ab15e6d419956cd
abdouazizsy <ziza97tiv@gmail.com>
2022-01-02 00:10:13 +0000

    creation service statistique

--- src/app/services/administrateur.service.ts
+++ src/app/services/administrateur.service.ts
@@ -1,30 +1,9 @@
-import { Administrateur } from './../modele/administrateur';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministrateurService {
 
-  private url= 'http://127.0.0.1:8000/api';
-
-
-  httpOption={
-    headers:new HttpHeaders({
-      'Content-Type':'Application/json',
-    }),
-  };
-
-  constructor(private http:HttpClient) {
-
-  }
-
-    authentification(login:String,mdp:String)
-    {
-      return   this.http.post<Administrateur>(this.url+'/administrateur/'+login+'/'+mdp,this.httpOption);
-    }
-
-
+  constructor() { }
 }
